590223a22b173df350de10a7a10f420eb6e7ac98
Tai Tran <taib2014783@student.ctu.edu.vn>
2023-04-11 00:22:30 +0700

    cart,contact,reg,delete,search,stylecss,...

---
+++ public/src/cart.php
@@ -0,0 +1,101 @@
+<?php include "../partialss/header.php"?>
+<?php 
+    // check user
+    if(!isset($_SESSION['user'])) {
+        $_SESSION['check_details'] = "<div class='error'>Bạn cần đăng nhập để xem chi tiết sản phẩm! </div>";
+        echo "<script>window.location = 'http://localhost/Project_ct275/public/src/login.php'</script>"; 
+    }
+
+?>
+<!-- body -->
+<main class="body">
+    <div class="container">
+        <div class="row-cart">
+            <div class="col-3">
+                <p1>Trang chủ</p1>
+            <a style="text-decoration: none; color: #20c997;"  href="">Giỏ hàng</a>
+            </div>
+            <hr>
+        </div>
+    </div>
+    <div class="container" style="display: flex; justify-content: center;">
+        <div class="row row-cart">
+            <form action="http://localhost/Project_ct275/public/src/handle_order.php" method="POST">
+                <div class="cart-details">
+                    <?php 
+                    // if(isset($_POST['submit'])) {
+                        try {
+                            $id_user = $_SESSION['user'];
+                            $query = "SELECT * from cart c join product p on c.id_product = p.id where id_user=$id_user";
+                            $sth = $pdo->query($query);
+                            $sth->execute();
+                            $count_cart = $sth->rowCount();
+                            $count = 0;
+                            $n = -1;
+                            if($count_cart == 0) {
+                                echo "<div style='position: relative; left: 40%; padding: 8px' class='error'>giỏ hàng trống!</div><hr>";
+                            }
+                            while($row = $sth->fetch()) {
+                                $count = $count + $row['quantity'] * $row['price'];
+                                $n++;
+                            ?>
+                            <div class="header-cart">
+                                <div class="img-cart" style="width: 20%">
+                                    <img src="../admin/upload_img/product/<?php echo $row['img']?>" style="width: 80%;" alt="">
+                                    <a href="http://localhost/Project_ct275/public/src/delete_cart.php?id=<?php echo $row['0']?>" style="text-decoration: none;">
+                                        <div class="delete">X</div>
+                                    </a>
+                                </div>
+                                <div class="title-cart" style="width: 50%">
+                                    <?php echo $row['titte']?>
+                                    <div class="gam-cart">
+                                        <?php echo $row['size']?>
+                                    </div>
+                                </div>
+                                <div class="price-cart" style="width: 30%">
+                                    <div style="color: #f30c28;">
+                                        <?php echo currency_format($row['quantity'] * $row['price'])?>                   
+                                    </div>
+                                    <div class="choosenumber">
+                                        <div class="minus1" onclick="Minus('<?php echo $n?>', <?php echo $row[0]?> )">
+                                            -
+                                        </div>
+                                        <input type="text" class="number1" value="<?php echo $row['quantity']?>" name="sluong">
+                                        <div class="plus1" onclick="Plus('<?php echo $n?>', <?php echo $row[0]?> )">
+                                            +
+                                        </div>
+                                        <input type="hidden" value="" name="sluong"></input>
+                                        <input type="hidden" value="<?php echo $id_user?>" name="id_user"></input>
+                                    </div>
+                                </div>
+                            </div>
+                        <hr>
+                        <?php
+                            }
+                        }catch(PDOException $e) {
+                            echo $e->getMessage();
+                        }
+                    ?>
+                    
+                    <!-- footer cart -->
+                    <div class="price-total">
+                        <div class="header-total">
+                            <div><strong>Tổng Tiền:</trong>
+                            </div>
+                            <div class="price-total-2" style="color: red;">
+                                <?php echo currency_format($count);?>
+                            </div>
+                            <input type="hidden" name="total" value="">
+                        </div>
+                        <button class="order" name="submit">MUA NGAY</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <br>
+    </div>
+</main>
+<script src="cart.js"></script>
+<hr>
+
+<?php include "../partialss/footer.php"?>
---
+++ public/src/reg.php
@@ -0,0 +1,115 @@
+<?php include "../partialss/header.php"?>
+    <main><div class="body" style="margin-bottom: 50px">
+            <div class="container">
+                <section class="vh-100 bg-image"
+  style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp');">
+  <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+    <div class="container h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+          <div class="card" style="border-radius: 15px;">
+            <div  class="card-body p-1 ">
+              <h1 class="text-uppercase text-center mb-2">Đăng ký tài khoản</h1>
+              <?php
+                if(isset($_SESSION['check_passwork'])) {
+                  echo $_SESSION['check_passwork'];
+                  unset($_SESSION['check_passwork']);
+                }
+              ?>
+              <form action="" method="POST" id="validation" style="padding-left: 40px;">
+              
+                <div class="form-outline">
+                  <label class="form-label" for="form3Example1cg">Họ Tên</label>
+                  <input style="font-size: x-small;"  type="text" id="form3Example1cg" class="form-control form-control-lg" name="fullname" required/>                  
+                </div>
+
+                <div class="form-outline">
+                  <label class="form-label" for="form3Example3cg">Tài khoản</label>
+                  <input style="font-size: x-small;"  type="text" id="form3Example3cg" class="form-control form-control-lg" name="account" required/>
+                </div>
+
+                <div class="form-outline">
+                  <label class="form-label" for="form3Example4cg">Mật khẩu</label>
+                  <input style="font-size: x-small;"  type="password" id="form3Example4cg" class="form-control form-control-lg" name="password" required/>
+                </div>
+
+                <div class="form-outline">
+                  <label class="form-label" for="form3Example4cdg">Nhập lại mật khẩu</label>
+                  <input style="font-size: x-small;"  type="password" id="form3Example4cdg" class="form-control form-control-lg" name="same_password" required/>
+                </div>
+
+                <div class="form-outline">
+                  <label class="form-label" for="form3Example4cd">Nhập số điện thoại</label>
+                  <input style="font-size: x-small;"  type="text" id="form3Example4cd" class="form-control form-control-lg" name="sdt" required/>
+                </div>
+
+                <div class="form-outline">
+                  <label class="form-label" for="form3Example4">Nhập tỉnh thành</label>
+                  <input style="font-size: x-small;" type="text" id="form3Example4" class="form-control form-control-lg" name="city" required/>
+                </div>
+                <div class="form-outline">
+                  <label class="form-label" for="form3Example4c">Nhập huyện</label>
+                  <input style="font-size: x-small;"  type="text" id="form3Example4c" class="form-control form-control-lg" name="district" required/>
+                </div>
+                <div class="form-check d-flex justify-content-center mb-3">
+                  <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3cg" name="agree" required/>
+                  <label class="form-check-label" for="form2Example3g">
+                    Tôi đồng ý tất cả điều khoản của <a href="#!" class="text-body"><u>đội dịch vụ</u></a>
+                  </label>
+                </div>
+
+                <div class="d-flex justify-content-center">
+                  <button type="submit" name="submit"
+                    class="btn btn-success btn-block btn-lg gradient-custom-4 text-white">Đăng ký</button>
+                </div>
+
+                <p class="text-center text-muted mb-0">Bạn đã có tài khoản? <a href="/Project_ct275/public/src/login.php"
+                    class="fw-bold text-body"><u>Đăng nhập tại đây!</u></a></p>
+
+              </form>
+              <?php
+                  if(isset($_POST['submit'])) {
+                      $fullname = $_POST['fullname'];
+                      $account = $_POST['account'];
+                      $password = md5($_POST['password']);
+                      $same_password = md5($_POST['same_password']);
+                      $phone = $_POST['sdt'];
+                      $city = $_POST['city'];
+                      $district = $_POST['district'];
+                      echo $password;
+                      echo $same_password;
+                      if($password != $same_password) {
+                        $_SESSION['check_passwork'] = "<div class='error'> Hai mật khẩu không khớp! </div>";
+                        echo "<script>window.location = 'http://localhost/Project_ct275/public/src/reg.php'</script>";
+                      }else{
+                        $query = "INSERT INTO users (account, password, fullname, phone, city, district) VALUES (?,?,?,?,?,?)";
+                        $sth = $pdo->prepare($query);
+                        $sth->execute([
+                          $account,
+                          $password,
+                          $fullname,
+                          $phone,
+                          $city,
+                          $district
+                        ]);
+                        if($sth == true) {
+                          $_SESSION['check_passwork'] = "<div class='success'> Đăng ký thành công </div>";
+                          echo "<script>window.location = 'http://localhost/Project_ct275/public/src/reg.php'</script>";
+                        }
+                      }
+                  }
+              ?>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+            </div>
+         </div>
+    </main>
+    <!-- footer -->
+    <?php include "../partialss/footer.php"?>
